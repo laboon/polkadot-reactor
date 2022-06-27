04923bd3565ddfa1cee5a16a0c55ebc9a9bfb8d1
laboon <laboon@gmail.com>
2022-06-27 11:59:18 +0200

    send

--- reactor.py
+++ reactor.py
@@ -5,11 +5,18 @@ from substrateinterface.exceptions import SubstrateRequestException
 
 NODE_WSS = "wss://kusama-rpc.polkadot.io"
 
+g_keypair = ""
+
+def vote():
+    global g_keypair
+    print("voting no!")
+
 def subscription_handler(events_obj, update_nr, subscription_id):
 
     for event in events_obj:
         if event.value["module_id"] == "System" and event.value["event_id"] == "Remarked":
             print('System.Remarked event found with attributes:', event.value['attributes'])
+            vote()
 
 
 ##########################################
@@ -19,18 +26,14 @@ def subscription_handler(events_obj, update_nr, subscription_id):
 
 # Check that a seed phrase is entered
 
-# if len(sys.argv) != 5:
-#     print("Arguments:  amount_in_plancks filename seed_phrase_in_quotes")
-#     sys.exit("Could not read arguments")
+if len(sys.argv) != 2:
+    print("Arguments: seed_phrase_in_quotes")
+    sys.exit("Could not read arguments")
 
-# print(sys.argv)
-# asset_id = sys.argv[1]
-# amount = sys.argv[2]
-# filename = sys.argv[3]
-# seed = sys.argv[4]
+print(sys.argv)
+seed = sys.argv[1]
 
-# Connect to node. By default this goes to Statemine, just change RPC endpoint to
-# Statemint to use that instead.
+# Connect to node. 
 
 print("Connecting to node...", end='')
 
@@ -39,6 +42,14 @@ substrate = SubstrateInterface(
     ss58_format=2,
     type_registry_preset='kusama'
 )
+
+# Generate keypair
+
+print("Generating keypair...", end='')
+
+g_keypair = Keypair.create_from_mnemonic(seed)
+
+print(" done!")
 
 
 result = substrate.query("System", "Events", [], subscription_handler=subscription_handler)
